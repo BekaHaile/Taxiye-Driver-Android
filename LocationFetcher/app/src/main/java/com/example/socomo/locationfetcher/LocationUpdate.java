499e7272c2621ce537a3c255f56345fdcaf068a1
Shankar <shankar@jugnoo.in>
2016-05-20 10:50:15 +0530

    feature_pubnub: extra files removed

--- LocationFetcher/app/src/main/java/com/example/socomo/locationfetcher/LocationUpdate.java
+++
@@ -1,7 +0,0 @@
-package com.example.socomo.locationfetcher;
-
-import android.location.Location;
-
-public interface LocationUpdate {
-	public void onLocationChanged(Location location, int priority);
-}
