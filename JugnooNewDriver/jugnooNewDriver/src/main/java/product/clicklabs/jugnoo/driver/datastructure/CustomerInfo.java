ee4696dc8b58a8dc1c0215b6039987f2ebae75ec
aneesh bansal <aneesh.bansal@mail.jugnoo.in>
2016-07-26 11:29:30 +0530

    fix_actual_time_distance: time and distance reset activated

--- JugnooNewDriver/jugnooNewDriver/src/main/java/product/clicklabs/jugnoo/driver/datastructure/CustomerInfo.java
+++ JugnooNewDriver/jugnooNewDriver/src/main/java/product/clicklabs/jugnoo/driver/datastructure/CustomerInfo.java
@@ -370,7 +370,12 @@ public class CustomerInfo {
 				JSONObject jObj = new JSONObject(Prefs.with(context).getString(Constants.SP_CUSTOMER_RIDE_DATAS_OBJECT, Constants.EMPTY_OBJECT));
 				if (jObj.has(String.valueOf(getEngagementId()))) {
 					JSONObject jc = jObj.getJSONObject(String.valueOf(getEngagementId()));
-					return distance - Double.parseDouble(jc.optString(Constants.KEY_DISTANCE, "0"));
+					double startDistance = Double.parseDouble(jc.optString(Constants.KEY_DISTANCE, "0"));
+					if(distance < startDistance){
+						return 0;
+					} else{
+						return distance - startDistance;
+					}
 				}
 			} catch (Exception e) {
 				e.printStackTrace();
@@ -410,19 +415,19 @@ public class CustomerInfo {
 		return System.currentTimeMillis() - startTime;
 	}
 
-//	public void resetStartRideTime(Context context) {
-//		try {
-//			JSONObject jObj = new JSONObject(Prefs.with(context).getString(Constants.SP_CUSTOMER_RIDE_DATAS_OBJECT, Constants.EMPTY_OBJECT));
-//			if (jObj.has(String.valueOf(getEngagementId()))) {
-//				JSONObject jc = jObj.getJSONObject(String.valueOf(getEngagementId()));
-//				jc.put(Constants.KEY_RIDE_TIME, System.currentTimeMillis());
-//				jObj.put(String.valueOf(getEngagementId()), jc);
-//				Prefs.with(context).save(Constants.SP_CUSTOMER_RIDE_DATAS_OBJECT, jObj.toString());
-//			}
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-//	}
+	public void resetStartRideTime(Context context) {
+		try {
+			JSONObject jObj = new JSONObject(Prefs.with(context).getString(Constants.SP_CUSTOMER_RIDE_DATAS_OBJECT, Constants.EMPTY_OBJECT));
+			if (jObj.has(String.valueOf(getEngagementId()))) {
+				JSONObject jc = jObj.getJSONObject(String.valueOf(getEngagementId()));
+				jc.put(Constants.KEY_RIDE_TIME, System.currentTimeMillis());
+				jObj.put(String.valueOf(getEngagementId()), jc);
+				Prefs.with(context).save(Constants.SP_CUSTOMER_RIDE_DATAS_OBJECT, jObj.toString());
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
 
 	public long getTotalWaitTime(long waitTime, Context context) {
