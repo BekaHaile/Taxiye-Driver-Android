05c986832e6122362cf1f2dfb4b4c9aa831f9807
aneesh bansal <aneesh.bansal@mail.jugnoo.in>
2016-11-08 18:55:10 +0530

    feature_ui_v3: bug fixes

--- JugnooNewDriver/jugnooNewDriver/src/main/java/product/clicklabs/jugnoo/driver/BootCompletedReceiver.java
+++ JugnooNewDriver/jugnooNewDriver/src/main/java/product/clicklabs/jugnoo/driver/BootCompletedReceiver.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 
 import product.clicklabs.jugnoo.driver.datastructure.GpsState;
+import product.clicklabs.jugnoo.driver.sticky.GeanieView;
 import product.clicklabs.jugnoo.driver.utils.Log;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
@@ -15,31 +16,32 @@ public class BootCompletedReceiver extends BroadcastReceiver {
     public void onReceive(final Context context, Intent arg1) {
         Log.w(TAG, "starting service...");
         String driverServiceRun = Database2.getInstance(context).getDriverServiceRun();
-        if (arg1.getAction().equals(Intent.ACTION_BOOT_COMPLETED) &&
-                (GpsState.GREATER_SIX.getOrdinal() == Database2.getInstance(context).getGpsState())) {
+        if (arg1.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
+			if (GpsState.GREATER_SIX.getOrdinal() == Database2.getInstance(context).getGpsState()) {
 
-            if(Database2.YES.equalsIgnoreCase(driverServiceRun)) {
-                GpsDistanceCalculator.lastLocationTime = System.currentTimeMillis() - 270000;
-                GpsDistanceCalculator.saveLastLocationTimeToSP(context, GpsDistanceCalculator.lastLocationTime);
+				if (Database2.YES.equalsIgnoreCase(driverServiceRun)) {
+					GpsDistanceCalculator.lastLocationTime = System.currentTimeMillis() - 270000;
+					GpsDistanceCalculator.saveLastLocationTimeToSP(context, GpsDistanceCalculator.lastLocationTime);
 
-                Intent i = context.getPackageManager().getLaunchIntentForPackage(context.getPackageName());
-                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                context.startActivity(i);
-                Database2.getInstance(context).updateGpsState(GpsState.ZERO_TWO.getOrdinal());
-            }
-        }
-        else if (arg1.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
-            try {
-                if (Database2.YES.equalsIgnoreCase(driverServiceRun)) {
-                    Database2.getInstance(context).updateDriverLastLocationTime();
-                    context.startService(new Intent(context, DriverLocationUpdateService.class));
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            } finally {
-                Database2.getInstance(context).close();
-            }
-        }
+					Intent i = context.getPackageManager().getLaunchIntentForPackage(context.getPackageName());
+					i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+					context.startActivity(i);
+					Database2.getInstance(context).updateGpsState(GpsState.ZERO_TWO.getOrdinal());
+				}
+			} else {
+				try {
+					if (Database2.YES.equalsIgnoreCase(driverServiceRun)) {
+						Database2.getInstance(context).updateDriverLastLocationTime();
+						context.startService(new Intent(context, DriverLocationUpdateService.class));
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				} finally {
+					Database2.getInstance(context).close();
+				}
+			}
+			context.startService(new Intent(context, GeanieView.class));
+		}
 
     }
 }
