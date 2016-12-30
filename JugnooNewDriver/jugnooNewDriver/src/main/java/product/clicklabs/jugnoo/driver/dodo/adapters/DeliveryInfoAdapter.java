8b61e9e1f29491ed359d70d2a811f4a437a82c49
aneesh bansal <aneesh.bansal@mail.jugnoo.in>
2016-12-30 11:11:36 +0530

    feature_delivery_ui_changes: delivery list UI modified

--- JugnooNewDriver/jugnooNewDriver/src/main/java/product/clicklabs/jugnoo/driver/dodo/adapters/DeliveryInfoAdapter.java
+++ JugnooNewDriver/jugnooNewDriver/src/main/java/product/clicklabs/jugnoo/driver/dodo/adapters/DeliveryInfoAdapter.java
@@ -13,6 +13,7 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 
+import product.clicklabs.jugnoo.driver.Data;
 import product.clicklabs.jugnoo.driver.HomeActivity;
 import product.clicklabs.jugnoo.driver.R;
 import product.clicklabs.jugnoo.driver.datastructure.CustomerInfo;
@@ -22,11 +23,15 @@ import product.clicklabs.jugnoo.driver.utils.ASSL;
 import product.clicklabs.jugnoo.driver.utils.Fonts;
 
 
-public class DeliveryInfoAdapter extends RecyclerView.Adapter<DeliveryInfoAdapter.ViewHolder> {
+public class DeliveryInfoAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
     private HomeActivity activity;
     private ArrayList<DeliveryInfo> deliveryInfos = new ArrayList<>();
     private Callback callback;
+
+	private static final int TYPE_HEADER = 3;
+	private static final int TYPE_FOOTER = 2;
+	private static final int TYPE_ITEM = 1;
 
     public DeliveryInfoAdapter(HomeActivity activity, ArrayList<DeliveryInfo> deliveryInfos, Callback callback) {
         this.activity = activity;
@@ -36,113 +41,140 @@ public class DeliveryInfoAdapter extends RecyclerView.Adapter<DeliveryInfoAdapte
 
 
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_delivery_info, parent, false);
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 
-        RecyclerView.LayoutParams layoutParams = new RecyclerView.LayoutParams(720, ViewGroup.LayoutParams.WRAP_CONTENT);
-        v.setLayoutParams(layoutParams);
+		if (viewType == TYPE_FOOTER) {
+			View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_delivery_list_end, parent, false);
 
-        ASSL.DoMagic(v);
-        return new ViewHolder(v, activity);
+			RecyclerView.LayoutParams layoutParams = new RecyclerView.LayoutParams(720, RecyclerView.LayoutParams.WRAP_CONTENT);
+			v.setLayoutParams(layoutParams);
+
+			ASSL.DoMagic(v);
+			return new ViewFooterHolder(v, activity);
+		} else if (viewType == TYPE_HEADER) {
+			View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_delivery_list_start, parent, false);
+
+			RecyclerView.LayoutParams layoutParams = new RecyclerView.LayoutParams(720, RecyclerView.LayoutParams.WRAP_CONTENT);
+			v.setLayoutParams(layoutParams);
+
+			ASSL.DoMagic(v);
+			return new ViewHeaderHolder(v, activity);
+		} else {
+
+			View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_delivery_info, parent, false);
+
+			RecyclerView.LayoutParams layoutParams = new RecyclerView.LayoutParams(720, ViewGroup.LayoutParams.WRAP_CONTENT);
+			v.setLayoutParams(layoutParams);
+
+			ASSL.DoMagic(v);
+			return new ViewListHolder(v, activity);
+		}
     }
 
 
+
     @Override
-    public void onBindViewHolder(final ViewHolder holder, int position) {
+    public void onBindViewHolder(final RecyclerView.ViewHolder viewHolder, int position) {
         try {
-            final DeliveryInfo deliveryInfo = getItem(position);
 
-            holder.textViewOrderIdValue.setText(String.valueOf(deliveryInfo.getIndex() + 1));
+			if (viewHolder instanceof ViewListHolder) {
+				ViewListHolder holder = (ViewListHolder) viewHolder;
 
-			if(deliveryInfo.getCustomerName().equalsIgnoreCase("")){
-				holder.textViewCustomerNameValue.setText(activity.getResources().getString(R.string.return_to_merchant));
-                holder.textViewOrderIdValue.setText("R");
-			} else {
-				holder.textViewCustomerNameValue.setText(deliveryInfo.getCustomerName());
-			}
+				final DeliveryInfo deliveryInfo = getItem(position);
 
-            holder.textViewCustomerDeliveryAddressValue.setText(deliveryInfo.getDeliveryAddress());
+				holder.textViewOrderIdValue.setText(String.valueOf(deliveryInfo.getIndex() +1));
 
-            holder.textViewOrderStatus.setVisibility(View.VISIBLE);
-            if(deliveryInfo.getStatus() == DeliveryStatus.PENDING.getOrdinal()){
-				holder.imageViewStatus.setBackgroundResource(R.drawable.delivery_pending_clock);
-
-				if(activity.getDeliveryPos() == deliveryInfo.getIndex()){
-					holder.linearLayoutCard1.setBackgroundResource(R.drawable.background_white_rounded_orange_bordered);
-					holder.textViewOrderStatus.setText(activity.getResources().getString(R.string.in_progress));
-					holder.textViewOrderStatus.setTextColor(activity.getResources().getColor(R.color.red_v2));
-					holder.imageViewStatus.setBackgroundResource(R.drawable.delivery_in_progress);
+				if (deliveryInfo.getCustomerName().equalsIgnoreCase("")) {
+					holder.textViewCustomerNameValue.setText(activity.getResources().getString(R.string.return_to_merchant));
+					holder.textViewOrderIdValue.setText("R");
 				} else {
-					holder.imageViewStatus.setBackgroundResource(R.drawable.delivery_pending_clock);
-					holder.textViewOrderStatus.setText(activity.getResources().getString(R.string.pending));
-					holder.textViewOrderStatus.setTextColor(activity.getResources().getColor(R.color.black_text_v2));
-					holder.linearLayoutCard1.setBackgroundResource(R.drawable.background_white_rounded_grey_alpha_bordered);
+					holder.textViewCustomerNameValue.setText(deliveryInfo.getCustomerName());
 				}
 
-            }
-            else {
-				holder.linearLayoutCard1.setBackgroundResource(R.drawable.background_grey_deivery_list_rounded_bordered);
-				holder.textViewCustomerNameValue.setTextColor(activity.getResources().getColor(R.color.black_text_v2_50op));
-				holder.textViewCustomerDeliveryAddressValue.setTextColor(activity.getResources().getColor(R.color.black_text_v2_50op));
-                if(deliveryInfo.getStatus() == DeliveryStatus.COMPLETED.getOrdinal()){
-                    holder.textViewOrderStatus.setText(activity.getResources().getString(R.string.delivered));
-                    holder.textViewOrderStatus.setTextColor(activity.getResources().getColor(R.color.green_delivery));
-					holder.imageViewStatus.setBackgroundResource(R.drawable.delivery_green_tick);
-                }
-                else if(deliveryInfo.getStatus() == DeliveryStatus.CANCELLED.getOrdinal()){
-                    holder.textViewOrderStatus.setText(activity.getResources().getString(R.string.failed));
-                    holder.textViewOrderStatus.setTextColor(activity.getResources().getColor(R.color.red_status));
-					holder.imageViewStatus.setBackgroundResource(R.drawable.delivery_red_cross);
-                }
-                else{
-                    holder.textViewOrderStatus.setVisibility(View.GONE);
-					holder.imageViewStatus.setVisibility(View.GONE);
-                }
-            }
+				holder.textViewCustomerDeliveryAddressValue.setText(deliveryInfo.getDeliveryAddress());
 
-            holder.rootLinear.setTag(position);
+				holder.textViewOrderStatus.setVisibility(View.VISIBLE);
+				if (deliveryInfo.getStatus() == DeliveryStatus.PENDING.getOrdinal()) {
+					holder.imageViewStatus.setBackgroundResource(R.drawable.delivery_pending_clock);
 
-            holder.rootLinear.setOnClickListener(new View.OnClickListener() {
+					if (activity.getDeliveryPos() == deliveryInfo.getIndex()) {
+						holder.linearLayoutCard1.setBackgroundResource(R.drawable.background_white_rounded_orange_bordered);
+						holder.textViewOrderStatus.setText(activity.getResources().getString(R.string.in_progress));
+						holder.textViewOrderStatus.setTextColor(activity.getResources().getColor(R.color.red_v2));
+						holder.imageViewStatus.setBackgroundResource(R.drawable.delivery_in_progress);
+					} else {
+						holder.imageViewStatus.setBackgroundResource(R.drawable.delivery_pending_clock);
+						holder.textViewOrderStatus.setText(activity.getResources().getString(R.string.pending));
+						holder.textViewOrderStatus.setTextColor(activity.getResources().getColor(R.color.black_text_v2));
+						holder.linearLayoutCard1.setBackgroundResource(R.drawable.background_white_rounded_grey_alpha_bordered);
+					}
 
-                @Override
-                public void onClick(View v) {
+				} else {
+					holder.linearLayoutCard1.setBackgroundResource(R.drawable.background_grey_deivery_list_rounded_bordered);
+					holder.textViewCustomerNameValue.setTextColor(activity.getResources().getColor(R.color.black_text_v2_50op));
+					holder.textViewCustomerDeliveryAddressValue.setTextColor(activity.getResources().getColor(R.color.black_text_v2_50op));
+					if (deliveryInfo.getStatus() == DeliveryStatus.COMPLETED.getOrdinal()) {
+						holder.textViewOrderStatus.setText(activity.getResources().getString(R.string.delivered));
+						holder.textViewOrderStatus.setTextColor(activity.getResources().getColor(R.color.green_delivery));
+						holder.imageViewStatus.setBackgroundResource(R.drawable.delivery_green_tick);
+					} else if (deliveryInfo.getStatus() == DeliveryStatus.CANCELLED.getOrdinal()) {
+						holder.textViewOrderStatus.setText(activity.getResources().getString(R.string.failed));
+						holder.textViewOrderStatus.setTextColor(activity.getResources().getColor(R.color.red_status));
+						holder.imageViewStatus.setBackgroundResource(R.drawable.delivery_red_cross);
+					} else {
+						holder.textViewOrderStatus.setVisibility(View.GONE);
+						holder.imageViewStatus.setVisibility(View.GONE);
+					}
+				}
+
+				holder.rootLinear.setTag(position);
+
+				holder.rootLinear.setOnClickListener(new View.OnClickListener() {
+
+					@Override
+					public void onClick(View v) {
 //                    int pos = (int) v.getTag();
 //                    callback.onClick(pos, getItem(pos));
 
-					for(int i=0; i< deliveryInfos.size(); i++){
-						deliveryInfos.get(i).setSate(false);
+						for (int i = 0; i < deliveryInfos.size(); i++) {
+							deliveryInfos.get(i).setSate(false);
+						}
+						if (deliveryInfo.getStatus() == DeliveryStatus.PENDING.getOrdinal()) {
+							deliveryInfo.setSate(true);
+						}
+						notifyDataSetChanged();
 					}
-					if(deliveryInfo.getStatus() == DeliveryStatus.PENDING.getOrdinal()){
-                        deliveryInfo.setSate(true);
+				});
+
+				if (deliveryInfo.isSate()) {
+					holder.linearLayoutSelection1.setVisibility(View.VISIBLE);
+				} else {
+					holder.linearLayoutSelection1.setVisibility(View.GONE);
+				}
+
+				holder.buttonSelect.setTag(position);
+				holder.buttonSelect.setOnClickListener(new View.OnClickListener() {
+					@Override
+					public void onClick(View v) {
+						int pos = (int) v.getTag();
+						callback.onClick(pos-1);
 					}
-					notifyDataSetChanged();
-                }
-            });
+				});
 
-            if(deliveryInfo.isSate()){
-                holder.linearLayoutSelection1.setVisibility(View.VISIBLE);
-            } else {
-                holder.linearLayoutSelection1.setVisibility(View.GONE);
-            }
+				holder.buttonCancel.setTag(position);
+				holder.buttonCancel.setOnClickListener(new View.OnClickListener() {
+					@Override
+					public void onClick(View v) {
+						int pos = (int) v.getTag();
+						callback.onClick(pos-1);
+					}
+				});
 
-			holder.buttonSelect.setTag(position);
-			holder.buttonSelect.setOnClickListener(new View.OnClickListener() {
-				@Override
-				public void onClick(View v) {
-					int pos = (int) v.getTag();
-					callback.onClick(pos);
-				}
-			});
-
-			holder.buttonCancel.setTag(position);
-			holder.buttonCancel.setOnClickListener(new View.OnClickListener() {
-				@Override
-				public void onClick(View v) {
-					int pos = (int) v.getTag();
-					callback.onClick(pos);
-				}
-			});
-
+			} else if (viewHolder instanceof ViewFooterHolder) {
+				ViewFooterHolder holder = (ViewFooterHolder) viewHolder;
+			} else if (viewHolder instanceof ViewHeaderHolder) {
+				ViewHeaderHolder holder = (ViewHeaderHolder) viewHolder;
+			}
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -155,21 +187,44 @@ public class DeliveryInfoAdapter extends RecyclerView.Adapter<DeliveryInfoAdapte
         if(deliveryInfos == null){
             return 0;
         } else{
-            return deliveryInfos.size();
+            return deliveryInfos.size()+2;
         }
     }
 
     public DeliveryInfo getItem(int position){
-        return deliveryInfos.get(position);
+
+		if (isPositionFooter(position) || isPositionHeader(position)) {
+			return null;
+		}
+
+        return deliveryInfos.get(position-1);
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
+	@Override
+	public int getItemViewType(int position){
+		if (position == deliveryInfos.size()+1) {
+			return TYPE_FOOTER;
+		} else if (position == 0) {
+			return TYPE_HEADER;
+		}
+		return TYPE_ITEM;
+	}
+
+	private boolean isPositionFooter(int position) {
+		return position == deliveryInfos.size()+1;
+	}
+
+	private boolean isPositionHeader(int position) {
+		return position == 0;
+	}
+
+    public class ViewListHolder extends RecyclerView.ViewHolder {
         public TextView textViewOrderIdValue, textViewCustomerNameValue, textViewCustomerDeliveryAddressValue, textViewOrderStatus;
         public RelativeLayout rootLinear;
         public LinearLayout linearLayoutSelection1, linearLayoutCard1;
 		public Button buttonSelect, buttonCancel;
 		public ImageView imageViewStatus, imageViewFakeBottom, imageViewVerticalLine, imageViewStatusCircle;
-        public ViewHolder(View convertView, Activity context) {
+        public ViewListHolder(View convertView, Activity context) {
             super(convertView);
             textViewOrderIdValue = (TextView) convertView.findViewById(R.id.textViewOrderIdValue);
             textViewOrderIdValue.setTypeface(Fonts.mavenRegular(context));
@@ -187,9 +242,41 @@ public class DeliveryInfoAdapter extends RecyclerView.Adapter<DeliveryInfoAdapte
 			imageViewStatus = (ImageView) convertView.findViewById(R.id.imageViewStatus);
             imageViewFakeBottom = (ImageView) convertView.findViewById(R.id.imageViewFakeBottom);
 //			imageViewVerticalLine = (ImageView) convertView.findViewById(R.id.imageViewVerticalLine);
-			imageViewStatusCircle = (ImageView) convertView.findViewById(R.id.imageViewStatusCircle);
+            imageViewStatusCircle = (ImageView) convertView.findViewById(R.id.imageViewStatusCircle);
+
         }
     }
+
+    public class ViewFooterHolder extends RecyclerView.ViewHolder {
+        public RelativeLayout relativeLayoutEnd;
+        public TextView textViewDeliveryStop;
+        public ImageView imageViewDeliverycircle2, imageViewDeliveryline2;
+
+        public ViewFooterHolder(View convertView, Activity context) {
+            super(convertView);
+            relativeLayoutEnd = (RelativeLayout) convertView.findViewById(R.id.relativeLayoutEnd);
+            textViewDeliveryStop = (TextView) convertView.findViewById(R.id.textViewDeliveryStop);
+            textViewDeliveryStop.setText(activity.getResources().getString(R.string.delivery_end));
+            imageViewDeliverycircle2 = (ImageView) convertView.findViewById(R.id.imageViewDeliverycircle2);
+            imageViewDeliveryline2 = (ImageView) convertView.findViewById(R.id.imageViewDeliveryline2);
+        }
+    }
+
+    public class ViewHeaderHolder extends RecyclerView.ViewHolder {
+        public RelativeLayout relativeLayoutStart;
+        public TextView textViewDeliveryStart;
+        public ImageView imageViewDeliverycircle, imageViewDeliveryline1;
+
+        public ViewHeaderHolder(View convertView, Activity context) {
+            super(convertView);
+            relativeLayoutStart = (RelativeLayout) convertView.findViewById(R.id.relativeLayoutStart);
+            textViewDeliveryStart = (TextView) convertView.findViewById(R.id.textViewDeliveryStart);
+            textViewDeliveryStart.setText(activity.getResources().getString(R.string.delivery_start));
+            imageViewDeliverycircle = (ImageView) convertView.findViewById(R.id.imageViewDeliverycircle);
+            imageViewDeliveryline1 = (ImageView) convertView.findViewById(R.id.imageViewDeliveryline1);
+        }
+    }
+
 
     public interface Callback{
 		void onClick(int position);
