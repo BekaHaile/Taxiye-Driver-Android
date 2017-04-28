6db36e79df947d14ba43071ccf55c89647e19ba4
aneeshbansaljugnoo <aneesh.bansal@mail.jugnoo.in>
2017-04-28 10:43:56 +0530

    feature_delivery_rate_card: 284 beta and ui changes

--- JugnooNewDriver/jugnooNewDriver/src/main/java/product/clicklabs/jugnoo/driver/retrofit/RestClient.java
+++ JugnooNewDriver/jugnooNewDriver/src/main/java/product/clicklabs/jugnoo/driver/retrofit/RestClient.java
@@ -63,7 +63,7 @@ public class RestClient {
 		RestAdapter.Builder builder = new RestAdapter.Builder()
 				.setEndpoint(Data.SERVER_URL)
 				.setClient(new Ok3Client(getOkHttpClient()))
-//				.setLog(fooLog)
+				.setLog(fooLog)
 				.setLogLevel(RestAdapter.LogLevel.FULL)
 				;
 
@@ -83,7 +83,7 @@ public class RestClient {
 			RestAdapter.Builder builder = new RestAdapter.Builder()
 					.setEndpoint(url)
 					.setClient(new Ok3Client(getOkHttpClient()))
-//					.setLog(fooLog)
+					.setLog(fooLog)
 					.setLogLevel(RestAdapter.LogLevel.FULL);
 
 			RestAdapter restAdapter = builder.build();
