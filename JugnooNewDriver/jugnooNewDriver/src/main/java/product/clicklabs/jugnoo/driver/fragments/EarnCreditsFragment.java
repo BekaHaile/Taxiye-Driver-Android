9eb6ef29edc70c170e51221410480de3b915c351
Parminder <parminder.saini@jungleworks.co>
2018-05-26 15:45:34 +0530

    feat_credits: UI Fixes

--- JugnooNewDriver/jugnooNewDriver/src/main/java/product/clicklabs/jugnoo/driver/fragments/EarnCreditsFragment.java
+++ JugnooNewDriver/jugnooNewDriver/src/main/java/product/clicklabs/jugnoo/driver/fragments/EarnCreditsFragment.java
@@ -10,6 +10,7 @@ import android.view.ViewGroup;
 
 import product.clicklabs.jugnoo.driver.R;
 import product.clicklabs.jugnoo.driver.listeners.DriverCreditsListener;
+import product.clicklabs.jugnoo.driver.utils.Utils;
 
 /**
  * Created by Parminder Saini on 25/05/18.
@@ -32,6 +33,8 @@ public class EarnCreditsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.fragment_earn_credits, container, false);
+        Utils.setTypeface(getActivity(),rootView.findViewById(R.id.tv_refer_driver),rootView.findViewById(R.id.tv_refer_customer)
+        ,rootView.findViewById(R.id.tv_advertise_us));
         rootView.findViewById(R.id.tv_refer_driver).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
