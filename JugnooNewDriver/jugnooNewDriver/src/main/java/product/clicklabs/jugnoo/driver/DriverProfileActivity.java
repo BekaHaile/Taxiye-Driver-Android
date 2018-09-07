3fd73eafffb578e3a9a1e9e40997f0eb2fda8db7
Geeta <geeta@jungleworks.com>
2018-09-07 15:56:57 +0530

    nickel ride:-profile_monthly_earnings_text added

--- JugnooNewDriver/jugnooNewDriver/src/main/java/product/clicklabs/jugnoo/driver/DriverProfileActivity.java
+++ JugnooNewDriver/jugnooNewDriver/src/main/java/product/clicklabs/jugnoo/driver/DriverProfileActivity.java
@@ -147,7 +147,7 @@ public class DriverProfileActivity extends BaseActivity {
         textViewmonthlyScore.setTypeface(Fonts.mavenMedium(this), Typeface.BOLD);
         textViewMonthlyText = (TextView) findViewById(R.id.textViewMonthlyText);
         textViewMonthlyText.setTypeface(Fonts.mavenRegular(this));
-        textViewMonthlyText.setText(getStringText(R.string.earnings));
+        textViewMonthlyText.setText(getStringText(R.string.profile_monthly_earnings_text));
         textViewRidesTakenText = (TextView) findViewById(R.id.textViewRidesTakenText);
         textViewRidesTakenText.setTypeface(Fonts.mavenRegular(this));
         textViewRidesTakenText.setText(getStringText(R.string.rides_taken));
