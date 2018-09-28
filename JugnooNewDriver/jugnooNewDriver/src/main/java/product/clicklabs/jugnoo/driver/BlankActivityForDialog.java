e3784b1952e9c0d408db6cc593ec27506fc95b38
Shankar <shankar@jungleworks.co>
2018-09-28 16:02:28 +0530

    wl_yourcabs: crash fix for BlankActivity for dialog

--- JugnooNewDriver/jugnooNewDriver/src/main/java/product/clicklabs/jugnoo/driver/BlankActivityForDialog.java
+++ JugnooNewDriver/jugnooNewDriver/src/main/java/product/clicklabs/jugnoo/driver/BlankActivityForDialog.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.Toast;
@@ -11,6 +12,7 @@ import android.widget.Toast;
 import product.clicklabs.jugnoo.driver.ui.DriverSplashActivity;
 import product.clicklabs.jugnoo.driver.utils.ASSL;
 import product.clicklabs.jugnoo.driver.utils.DialogPopup;
+import product.clicklabs.jugnoo.driver.utils.Utils;
 
 public class BlankActivityForDialog extends Activity{
 
@@ -22,54 +24,60 @@ public class BlankActivityForDialog extends Activity{
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_blank_for_dialog);
-		
-		relative = (LinearLayout) findViewById(R.id.relative);
-		new ASSL(BlankActivityForDialog.this, relative, 1134, 720, false);
+		try {
+			super.onCreate(savedInstanceState);
+			setContentView(R.layout.activity_blank_for_dialog);
 
-        relative.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
+			relative = (LinearLayout) findViewById(R.id.relative);
+			new ASSL(BlankActivityForDialog.this, relative, 1134, 720, false);
 
-        if(getIntent().hasExtra("wakeUp")){
-			Intent homeScreen = new Intent(this, DriverSplashActivity.class);
-			startActivity(homeScreen);
-			finish();
-        }
+			relative.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					finish();
+				}
+			});
 
-        if(getIntent().hasExtra("message1")){
-            message = getIntent().getStringExtra("message1");
-            new Handler().postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    DialogPopup.alertPopupWithListener(BlankActivityForDialog.this, "", message,
-                            new View.OnClickListener() {
-                                @Override
-                                public void onClick(View view) {
-                                    finish();
-                                }
-                            });
-                }
-            }, 200);
-        }
-        else if(getIntent().hasExtra("message2")){
-            message = getIntent().getStringExtra("message2");
-            new Handler().postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    DialogPopup.dialogBanner(BlankActivityForDialog.this, message);
-                }
-            }, 200);
-        }
-        else{
-            finish();
-        }
+			if(getIntent().hasExtra("wakeUp")){
+				Intent homeScreen = new Intent(this, DriverSplashActivity.class);
+				startActivity(homeScreen);
+				finish();
+			}
 
-
+			if(getIntent().hasExtra("message1") || getIntent().hasExtra("restart_phone")){
+				message = getIntent().getStringExtra("message1");
+				if (TextUtils.isEmpty(message)) {
+					message = getIntent().getStringExtra("restart_phone");
+				}
+				new Handler().postDelayed(new Runnable() {
+					@Override
+					public void run() {
+						DialogPopup.alertPopupWithListener(BlankActivityForDialog.this, "", message,
+								new View.OnClickListener() {
+									@Override
+									public void onClick(View view) {
+										finish();
+									}
+								});
+					}
+				}, 200);
+			}
+			else if(getIntent().hasExtra("message2")){
+				message = getIntent().getStringExtra("message2");
+				new Handler().postDelayed(new Runnable() {
+					@Override
+					public void run() {
+						DialogPopup.dialogBanner(BlankActivityForDialog.this, message);
+					}
+				}, 200);
+			}
+			else{
+				finish();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			Utils.showToast(this, getResources().getString(R.string.restart_phone));
+		}
 
 
 	}
