cd7bd737dc873f23add1c495271c52fd3d554149
Shankar <shankar@jungleworks.co>
2019-05-30 15:58:58 +0530

    f_google_direction_at_zero_distance: initial code before testing

--- JugnooNewDriver/jugnooNewDriver/src/main/java/product/clicklabs/jugnoo/driver/datastructure/FlagRideStatus.java
+++ JugnooNewDriver/jugnooNewDriver/src/main/java/product/clicklabs/jugnoo/driver/datastructure/FlagRideStatus.java
@@ -3,7 +3,8 @@ package product.clicklabs.jugnoo.driver.datastructure;
 public enum FlagRideStatus {
 	END_RIDE_ADDED_DISPLACEMENT(0),
 	END_RIDE_ADDED_DISTANCE(1),
-	END_RIDE_ADDED_COMP_DIST(2)
+	END_RIDE_ADDED_COMP_DIST(2),
+	END_RIDE_ADDED_GOOGLE_DISTANCE(3)
 	;
 
 	private int ordinal;
