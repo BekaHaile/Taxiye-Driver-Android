477cfa32bbdddea15d7d24fbde26035625054c61
Shankar <shankar@jungleworks.co>
2019-07-17 15:35:08 +0530

    wl_dev: driver timeout comments

--- JugnooNewDriver/jugnooNewDriver/src/main/java/product/clicklabs/jugnoo/driver/DriverTimeoutCheck.java
+++ JugnooNewDriver/jugnooNewDriver/src/main/java/product/clicklabs/jugnoo/driver/DriverTimeoutCheck.java
@@ -17,25 +17,26 @@ public class DriverTimeoutCheck {
 		try {
 			if (highPenalty>0) {
 				if(highPenalty ==1) {
-					penaltyFactor = Prefs.with(context).getInt(SPLabels.DRIVER_TIMEOUT_FACTOR, 1);
+					penaltyFactor = Prefs.with(context).getInt(SPLabels.DRIVER_TIMEOUT_FACTOR, 1); //customer_cancel_timeout_factor (ride cancel by customer)
 				}else if(highPenalty ==2){
-					penaltyFactor = Prefs.with(context).getInt(SPLabels.DRIVER_TIMEOUT_FACTOR_HIGH, 1);
+					penaltyFactor = Prefs.with(context).getInt(SPLabels.DRIVER_TIMEOUT_FACTOR_HIGH, 1); //driver_cancel_timeout_factor (ride cancel by driver)
 				}
 				Prefs.with(context).save(SPLabels.BUFFER_TIMEOUT_VALUE, System.currentTimeMillis() - 5000);
 			}
 
-			if ((Prefs.with(context).getInt(SPLabels.DRIVER_TIMEOUT_FLAG, 0) == 1)) {
+			if ((Prefs.with(context).getInt(SPLabels.DRIVER_TIMEOUT_FLAG, 0) == 1)) {//penalise_driver_timeout (feature control flag)
 				if (System.currentTimeMillis() >
 						Prefs.with(context).getLong(SPLabels.BUFFER_TIMEOUT_VALUE, System.currentTimeMillis() - 5000)) {
 					Prefs.with(context).save(SPLabels.INGNORE_RIDEREQUEST_COUNT, Prefs.with(context).getInt(SPLabels.INGNORE_RIDEREQUEST_COUNT, 0) + penaltyFactor);
 					Database2.getInstance(context).insertPenalityData(String.valueOf(System.currentTimeMillis()), penaltyFactor);
 					Prefs.with(context).save(SPLabels.BUFFER_TIMEOUT_VALUE,
-							System.currentTimeMillis() + Prefs.with(context).getLong(SPLabels.BUFFER_TIMEOUT_PERIOD, 0));
+							System.currentTimeMillis() + Prefs.with(context).getLong(SPLabels.BUFFER_TIMEOUT_PERIOD, 0)); //timeout_counter_buffer (time frame in which multiple request missed/canceled events will be considered one only)
 				}
 				Log.i("TimeOutAlarmReceiver", "1");
 
-				long timeDiff = System.currentTimeMillis() - Prefs.with(context).getLong(SPLabels.DRIVER_TIMEOUT_TTL, 0);
-				if (Prefs.with(context).getInt(SPLabels.MAX_INGNORE_RIDEREQUEST_COUNT, 0) <= Database2.getInstance(context).getPenalityData(String.valueOf(timeDiff))) {
+				long timeDiff = System.currentTimeMillis() - Prefs.with(context).getLong(SPLabels.DRIVER_TIMEOUT_TTL, 0); //timeout_ttl (till how much time any penalty will be valid)
+				if (Prefs.with(context).getInt(SPLabels.MAX_INGNORE_RIDEREQUEST_COUNT, 0) //max_allowed_timeouts
+						<= Database2.getInstance(context).getPenalityData(String.valueOf(timeDiff))) {
 					Intent timeoutIntent = new Intent(context, DriverTimeoutIntentService.class);
 					context.startService(timeoutIntent);
 				}
