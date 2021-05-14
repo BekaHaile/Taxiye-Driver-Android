9a58fee79010bedc6e6e3e1c6c158e7930aa6419
Beka Haile <barbeka.tola@gmail.com>
2021-05-14 14:38:34 +0300

    Other option removed from enum and strings

--- JugnooNewDriver/jugnooNewDriver/src/main/java/product/clicklabs/jugnoo/driver/datastructure/GenderValues.kt
+++ JugnooNewDriver/jugnooNewDriver/src/main/java/product/clicklabs/jugnoo/driver/datastructure/GenderValues.kt
@@ -3,7 +3,6 @@ package product.clicklabs.jugnoo.driver.datastructure
 enum class GenderValues(val type: Int){
     MALE(1),
     FEMALE(2),
-    OTHER(3)
 }
 
 class Gender(val type:Int, val name:String){
