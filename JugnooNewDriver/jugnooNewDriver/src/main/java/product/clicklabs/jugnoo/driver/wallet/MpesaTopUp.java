e590ae0caf9b8ba409146b914a30f78917ef72d7
Beka Haile <barbeka.tola@gmail.com>
2021-06-10 10:45:27 +0300

    Phone number and country code picker added to mpesa

--- JugnooNewDriver/jugnooNewDriver/src/main/java/product/clicklabs/jugnoo/driver/wallet/MpesaTopUp.java
+++ JugnooNewDriver/jugnooNewDriver/src/main/java/product/clicklabs/jugnoo/driver/wallet/MpesaTopUp.java
@@ -1,25 +1,36 @@
 package product.clicklabs.jugnoo.driver.wallet;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.picker.Country;
+import com.picker.CountryPicker;
+import com.picker.OnCountryPickerListener;
 
 import product.clicklabs.jugnoo.driver.Data;
 import product.clicklabs.jugnoo.driver.R;
 import product.clicklabs.jugnoo.driver.utils.BaseActivity;
 import product.clicklabs.jugnoo.driver.utils.Fonts;
 import product.clicklabs.jugnoo.driver.utils.Prefs;
+import product.clicklabs.jugnoo.driver.utils.Utils;
 
-public class MpesaTopUp extends BaseActivity {
+public class MpesaTopUp extends BaseActivity implements OnCountryPickerListener<Country> {
 
     Button buttonDone, fiftyButton, hundredButton, hundredFiftyButton;
     ImageView backBtn;
     boolean isTopUp;
-    EditText editAmount;
+    EditText editAmount, edtPhoneNo;
     String currency = Data.userData.getCurrency() + " %s";
+
+    private TextView tvCountryCode;
+    private CountryPicker countryPicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +43,40 @@ public class MpesaTopUp extends BaseActivity {
         TextView title = (TextView) findViewById(R.id.title);
         title.setTypeface(Fonts.mavenRegular(this));
         title.setText(R.string.mpesa);
+
+        tvCountryCode = (TextView) findViewById(R.id.tvCountryCode);
+        tvCountryCode.setText(Utils.getCountryCode(this));
+        tvCountryCode.setOnClickListener(countryCodeClickListener);
+        countryPicker =
+                new CountryPicker.Builder().with(this)
+                        .listener(this)
+                        .build();
+
+        edtPhoneNo = (EditText) findViewById(R.id.edtPhoneNo);
+        edtPhoneNo.setText(Utils.retrievePhoneNumberTenChars(Data.userData.getCountryCode(), Data.userData.phoneNo));
+        edtPhoneNo.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (editable.toString().startsWith("0")) {
+                    if (editable.toString().length() > 1) {
+                        edtPhoneNo.setText(editable.toString().substring(1));
+                    } else {
+                        edtPhoneNo.setText("");
+                    }
+                    Toast.makeText(MpesaTopUp.this, MpesaTopUp.this.getString(R.string.number_should_not_start_with_zero), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
         editAmount = (EditText) findViewById(R.id.editAmount);
 
@@ -84,5 +129,17 @@ public class MpesaTopUp extends BaseActivity {
                 overridePendingTransition(R.anim.left_in, R.anim.left_out);
             }
         });
+    }
+
+    View.OnClickListener countryCodeClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            countryPicker.showDialog(getSupportFragmentManager());
+        }
+    };
+
+    @Override
+    public void onSelectCountry(Country country) {
+        tvCountryCode.setText(country.getDialCode());
     }
 }
